20f4070a4673eddb8d9845a32d9c7496b5e65574
alialeem01 <ali.aleem.1892@gmail.com>
2026-08-21 10:08:40 +0500

    Fix duplicate urlpatterns in parcels/urls.py

--- parcels/urls.py
+++ parcels/urls.py
@@ -3,14 +3,6 @@ from . import views
 
 urlpatterns = [
     path('track/<str:cn>/', views.track_parcel, name='track_parcel'),
-]
-
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('track/<str:cn>/', views.track_parcel, name='track_parcel'),
     path('orders/', views.list_my_orders, name='list_my_orders'),
+    path('orders/book/', views.book_order, name='book_order'),
 ]
-
-path('orders/book/', views.book_order, name='book_order'),
