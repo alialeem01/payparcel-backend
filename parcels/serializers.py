513f1691d98c5ee3ff673dd062528eef9f4ec295
alialeem01 <ali.aleem.1892@gmail.com>
2026-08-23 01:28:55 +0500

    Add more fields to orders API to prevent frontend filter crash

--- parcels/serializers.py
+++ parcels/serializers.py
@@ -6,10 +6,14 @@ class TrackingSerializer(serializers.ModelSerializer):
     status = serializers.SerializerMethodField()
     destination = serializers.SerializerMethodField()
     consignee = serializers.SerializerMethodField()
+    consignee_phone = serializers.SerializerMethodField()
+    order_number = serializers.SerializerMethodField()
+    service_type = serializers.SerializerMethodField()
 
     class Meta:
         model = CustomerParcel
-        fields = ['cn', 'status', 'destination', 'consignee', 'shipment_date', 'shipper_name', 'delivery_date']
+        fields = ['cn', 'status', 'destination', 'consignee', 'consignee_phone',
+                   'order_number', 'service_type', 'shipment_date', 'shipper_name', 'delivery_date']
 
     def get_status(self, obj):
         return obj.status or ''
@@ -19,3 +23,12 @@ class TrackingSerializer(serializers.ModelSerializer):
 
     def get_consignee(self, obj):
         return obj.consignee or ''
+
+    def get_consignee_phone(self, obj):
+        return obj.consignee_phone or ''
+
+    def get_order_number(self, obj):
+        return obj.order_number or ''
+
+    def get_service_type(self, obj):
+        return obj.service_type or ''
