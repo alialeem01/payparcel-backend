a43f89479504d95d6f3f7e14eae76c11c7442724
alialeem01 <ali.aleem.1892@gmail.com>
2026-08-23 03:08:27 +0500

    Add address field to Destination

--- locations/models.py
+++ locations/models.py
@@ -3,19 +3,20 @@ from django.db import models
 class Destination(models.Model):
     STATUS_CHOICES = [('Active', 'Active'), ('Inactive', 'Inactive')]
     ZONE_CHOICES = [
-    ('With in City', 'With in City'),
-    ('Different', 'Different'),
-    ('Same', 'Same'),
-    ('Zone 1', 'Zone 1'),
-    ('Zone 2', 'Zone 2'),
-    ('Zone 3', 'Zone 3'),
-    ('Zone 4', 'Zone 4'),
-    ('Zone 5', 'Zone 5'),
-    ('Zone 6', 'Zone 6'),
-    ('Zone 7', 'Zone 7'),
-]
+        ('With in City', 'With in City'),
+        ('Different', 'Different'),
+        ('Same', 'Same'),
+        ('Zone 1', 'Zone 1'),
+        ('Zone 2', 'Zone 2'),
+        ('Zone 3', 'Zone 3'),
+        ('Zone 4', 'Zone 4'),
+        ('Zone 5', 'Zone 5'),
+        ('Zone 6', 'Zone 6'),
+        ('Zone 7', 'Zone 7'),
+    ]
 
     name = models.CharField(max_length=150)
+    address = models.TextField(blank=True, null=True)
     short_name = models.CharField(max_length=50, blank=True, null=True)
     location = models.CharField(max_length=100, blank=True, null=True, help_text="Latitude,Longitude")
     zone = models.CharField(max_length=20, choices=ZONE_CHOICES, blank=True, null=True)
