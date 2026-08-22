6e7a7c5c7a32b5b9b4f9053e969978a3c8901a04
alialeem01 <ali.aleem.1892@gmail.com>
2026-08-23 01:14:10 +0500

    Ensure orders API never returns null for status/destination/consignee

--- parcels/serializers.py
+++ parcels/serializers.py
@@ -3,7 +3,19 @@ from .models import CustomerParcel
 
 class TrackingSerializer(serializers.ModelSerializer):
     shipper_name = serializers.CharField(source='shipper.customer_name', read_only=True)
+    status = serializers.SerializerMethodField()
+    destination = serializers.SerializerMethodField()
+    consignee = serializers.SerializerMethodField()
 
     class Meta:
         model = CustomerParcel
         fields = ['cn', 'status', 'destination', 'consignee', 'shipment_date', 'shipper_name', 'delivery_date']
+
+    def get_status(self, obj):
+        return obj.status or ''
+
+    def get_destination(self, obj):
+        return obj.destination or ''
+
+    def get_consignee(self, obj):
+        return obj.consignee or ''
