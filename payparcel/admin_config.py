138ab6df4c2047d677ebe27e4fe4522603a09c56
alialeem01 <ali.aleem.1892@gmail.com>
2026-08-23 11:35:01 +0500

    Custom sidebar order, fix sidebar to stay left

--- payparcel/admin_config.py
+++ payparcel/admin_config.py
@@ -1,5 +1,18 @@
 from django.contrib import admin
 
+CUSTOM_ORDER = [
+    'customer',       # 01 Customers
+    'customerparcel',  # 03 Customer Parcels
+    'pickupsheet',     # 03 Pickup Sheet
+    'loadsheet',       # 04 Loadsheet
+    'manifest',        # 04 Manifest
+    'deliverysheet',   # 06 Delivery Sheet
+    'invoice',         # 07 Customer Payment Invoices
+    'destination',     # Destinations
+    'branch',          # Branches
+    'statusnarration', # Status Narration List
+]
+
 def get_app_list(self, request, app_label=None):
     app_dict = self._build_app_dict(request)
     if not app_dict:
@@ -9,7 +22,13 @@ def get_app_list(self, request, app_label=None):
     for app in app_dict.values():
         all_models.extend(app['models'])
 
-    all_models.sort(key=lambda x: x['name'])
+    def sort_key(model):
+        object_name = model['object_name'].lower()
+        if object_name in CUSTOM_ORDER:
+            return CUSTOM_ORDER.index(object_name)
+        return 999
+
+    all_models.sort(key=sort_key)
 
     merged_app = {
         'name': 'App',
