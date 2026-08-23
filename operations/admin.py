a6e9713b478c0f59bcf32965a8d9bb1c41d9c364
alialeem01 <ali.aleem.1892@gmail.com>
2026-08-23 12:51:48 +0500

    Replace bulk actions dropdown with row-level Edit/Delete links

--- operations/admin.py
+++ operations/admin.py
@@ -1,36 +1,35 @@
 from django.contrib import admin
+from django.utils.html import format_html
+from django.urls import reverse
 from .models import PickupSheet, Manifest, DeliverySheet
+
 
 @admin.register(PickupSheet)
 class PickupSheetAdmin(admin.ModelAdmin):
-    list_display = ('sheet_number', 'rider', 'shipper', 'loadsheet', 'total_pickup_parcel', 'user', 'branch', 'pickup_status', 'qr_code', 'date')
-    list_editable = ('pickup_status',)
-    filter_horizontal = ('parcels',)
-    readonly_fields = ('sheet_number', 'total_pickup_parcel', 'user', 'branch', 'qr_code', 'last_update', 'date')
+    list_display = ('sheet_number', 'rider', 'shipper', 'loadsheet', 'total_pickup_parcel', 'user', 'branch', 'pickup_status', 'row_actions')
+    readonly_fields = ('sheet_number',)
     search_fields = ('sheet_number',)
     list_filter = ('pickup_status', 'branch')
+    actions = None  # removes the bulk action dropdown entirely
 
-    fields = ('rider', 'shipper', 'parcels', 'user', 'branch', 'total_pickup_parcel', 'pickup_status', 'loadsheet', 'qr_code', 'last_update', 'date')
-
-    def save_model(self, request, obj, form, change):
-        if not obj.user:
-            obj.user = request.user.username.upper()
-        super().save_model(request, obj, form, change)
-
-    def save_related(self, request, form, formsets, change):
-        super().save_related(request, form, formsets, change)
-        for parcel in form.instance.parcels.all():
-            parcel.status = 'Picked'
-            parcel.save()
+    def row_actions(self, obj):
+        edit_url = reverse('admin:operations_pickupsheet_change', args=[obj.pk])
+        delete_url = reverse('admin:operations_pickupsheet_delete', args=[obj.pk])
+        return format_html(
+            '<a href="{}">Edit</a> | <a href="{}">Delete</a>',
+            edit_url, delete_url
+        )
+    row_actions.short_description = 'Actions'
 
 
 @admin.register(Manifest)
 class ManifestAdmin(admin.ModelAdmin):
-    list_display = ('code', 'user', 'branch', 'origin', 'destination', 'mode_of_transportation', 'seal_no', 'total_weight', 'total_pcs', 'total_parcel', 'date')
+    list_display = ('code', 'user', 'branch', 'origin', 'destination', 'mode_of_transportation', 'seal_no', 'total_weight', 'total_pcs', 'total_parcel', 'date', 'row_actions')
     filter_horizontal = ('parcels',)
     readonly_fields = ('code', 'date')
     search_fields = ('code',)
     list_filter = ('mode_of_transportation', 'branch')
+    actions = None
 
     def save_related(self, request, form, formsets, change):
         super().save_related(request, form, formsets, change)
@@ -39,14 +38,24 @@ class ManifestAdmin(admin.ModelAdmin):
             parcel.status = 'In Transit'
             parcel.save()
 
+    def row_actions(self, obj):
+        edit_url = reverse('admin:operations_manifest_change', args=[obj.pk])
+        delete_url = reverse('admin:operations_manifest_delete', args=[obj.pk])
+        return format_html(
+            '<a href="{}">Edit</a> | <a href="{}">Delete</a>',
+            edit_url, delete_url
+        )
+    row_actions.short_description = 'Actions'
+
 
 @admin.register(DeliverySheet)
 class DeliverySheetAdmin(admin.ModelAdmin):
-    list_display = ('ds_number', 'user', 'branch', 'rider', 'cn_list', 'sheet_status', 'origin', 'area', 'total_parcels', 'total_cod', 'date')
+    list_display = ('ds_number', 'user', 'branch', 'rider', 'cn_list', 'sheet_status', 'origin', 'area', 'total_parcels', 'total_cod', 'date', 'row_actions')
     filter_horizontal = ('parcels',)
     readonly_fields = ('ds_number', 'date')
     search_fields = ('ds_number',)
     list_filter = ('sheet_status', 'branch')
+    actions = None
 
     def save_related(self, request, form, formsets, change):
         super().save_related(request, form, formsets, change)
@@ -55,3 +64,12 @@ class DeliverySheetAdmin(admin.ModelAdmin):
             for parcel in form.instance.parcels.all():
                 parcel.status = 'Delivered'
                 parcel.save()
+
+    def row_actions(self, obj):
+        edit_url = reverse('admin:operations_deliverysheet_change', args=[obj.pk])
+        delete_url = reverse('admin:operations_deliverysheet_delete', args=[obj.pk])
+        return format_html(
+            '<a href="{}">Edit</a> | <a href="{}">Delete</a>',
+            edit_url, delete_url
+        )
+    row_actions.short_description = 'Actions'
