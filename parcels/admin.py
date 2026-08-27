696a5cdb563f3a33c07b0fe64a002b0d75f56363
alialeem01 <ali.aleem.1892@gmail.com>
2026-08-27 16:01:57 +0500

    Fix admin.py content - ensure saved version matches intended fix

--- parcels/admin.py
+++ parcels/admin.py
@@ -1,3 +1,5 @@
+from django.utils.html import format_html
+from django.urls import reverse
 from django.contrib import admin
 from django.contrib import messages
 from django.shortcuts import render
@@ -9,17 +11,16 @@ from .models import CustomerParcel, StatusNarration
 
 @admin.register(CustomerParcel)
 class CustomerParcelAdmin(ModelAdmin):
-    list_display = ('cn', 'shipper', 'order_number', 'status', 'customer_payment_status', 'destination',
-                     'delivery_rider_service_provider', 'cod', 'net_total', 'branch', 'payment_status_display', 'active')
+    list_display = ('cn', 'shipper', 'order_number', 'status', 'customer_payment_status', 'city',
+                     'cod', 'net_total', 'branch', 'active', 'row_actions')
     list_editable = ('status', 'customer_payment_status')
     search_fields = ('cn', 'order_number', 'consignee', 'api_tracking_no')
     list_filter_submit = True
     list_filter = (
+        ('city', DropdownFilter),
         ('status', ChoicesDropdownFilter),
         'active',
         ('customer_payment_status', ChoicesDropdownFilter),
-        ('tpl_payment_status', ChoicesDropdownFilter),
-        ('destination', DropdownFilter),
     )
     readonly_fields = ('cn', 'net_total', 'last_update', 'shipment_date', 'tracking_qr_code')
     actions = ['assign_to_rider']
@@ -27,14 +28,15 @@ class CustomerParcelAdmin(ModelAdmin):
     fieldsets = (
         ('Parcel Details', {
             'fields': (
-                ('cn', 'shipper', 'destination'),
-                ('delivery_rider_service_provider', 'api_sp_type'),
-                ('api_tracking_no', 'tpl_payment_status', 'customer_payment_status'),
-                ('assigned_rider', 'tracking_qr_code'),
+                ('cn', 'shipper'),
+                ('city', 'customer_payment_status'),
+                ('tracking_qr_code'),
                 ('consignee', 'consignee_phone'),
                 ('alternate_phone', 'order_number', 'issue_destination'),
-                ('cod', 'rts_cod', 'parcel_declared_value', 'discount'),
-                ('service_type', 'parcel_weight', 'number_of_pieces', 'replace'),
+                ('cod', 'rts_cod'),
+                ('parcel_declared_value', 'discount'),
+                ('service_type', 'parcel_weight'),
+                ('number_of_pieces', 'replace'),
                 ('product', 'instructions'),
                 ('flyer_size', 'flyer_qty'),
                 'address',
@@ -43,7 +45,8 @@ class CustomerParcelAdmin(ModelAdmin):
                 ('active', 'status', 'reason'),
                 'proof_image',
                 ('delivery_date', 'delivery_time', 'share'),
-                ('order_from', 'store_order_number', 'loadsheet', 'customer_loadsheet'),
+                ('order_from', 'store_order_number'),
+                ('loadsheet', 'customer_loadsheet'),
                 ('last_update', 'shipment_date'),
                 ('branch', 'user'),
             )
@@ -51,7 +54,8 @@ class CustomerParcelAdmin(ModelAdmin):
         ('Total Charges', {
             'fields': (
                 'rate_calculation_mode',
-                ('total_gst', 'total_feul_tax', 'total_return', 'flyer_charges'),
+                ('total_gst', 'total_feul_tax'),
+                ('total_return', 'flyer_charges'),
                 ('third_party_charge', 'tpl_net_total'),
                 ('delivery_charge', 'net_total'),
             )
@@ -64,9 +68,10 @@ class CustomerParcelAdmin(ModelAdmin):
         }),
     )
 
-    def payment_status_display(self, obj):
-        return obj.tpl_payment_status
-    payment_status_display.short_description = 'Payment Status'
+    def row_actions(self, obj):
+        view_url = reverse('track_parcel', args=[obj.cn])
+        return format_html('<a href="{}" target="_blank">View</a>', view_url)
+    row_actions.short_description = 'Actions'
 
     def assign_to_rider(self, request, queryset):
         from riders.models import Rider
