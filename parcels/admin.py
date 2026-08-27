f97847757932d14819a95ddf07275ad26c9e20c0
alialeem01 <ali.aleem.1892@gmail.com>
2026-08-27 15:57:54 +0500

    Fix syntax error: row_actions method was pasted inside list_filter tuple

--- parcels/admin.py
+++ parcels/admin.py
@@ -1,5 +1,3 @@
-from django.utils.html import format_html
-from django.urls import reverse
 from django.contrib import admin
 from django.contrib import messages
 from django.shortcuts import render
@@ -11,21 +9,17 @@ from .models import CustomerParcel, StatusNarration
 
 @admin.register(CustomerParcel)
 class CustomerParcelAdmin(ModelAdmin):
-    list_display = ('cn', 'shipper', 'order_number', 'status', 'customer_payment_status', 'city',
-                 'cod', 'net_total', 'branch', 'active', 'row_actions')
+    list_display = ('cn', 'shipper', 'order_number', 'status', 'customer_payment_status', 'destination',
+                     'delivery_rider_service_provider', 'cod', 'net_total', 'branch', 'payment_status_display', 'active')
     list_editable = ('status', 'customer_payment_status')
     search_fields = ('cn', 'order_number', 'consignee', 'api_tracking_no')
     list_filter_submit = True
-    list_filter = ( ('city', DropdownFilter),
+    list_filter = (
         ('status', ChoicesDropdownFilter),
         'active',
         ('customer_payment_status', ChoicesDropdownFilter),
-        ('city', DropdownFilter)
-    def row_actions(self, obj):
-        view_url = reverse('track_parcel', args=[obj.cn])
-        return format_html('<a href="{}" target="_blank">View</a>', view_url)
-    row_actions.short_description = 'Actions'
-
+        ('tpl_payment_status', ChoicesDropdownFilter),
+        ('destination', DropdownFilter),
     )
     readonly_fields = ('cn', 'net_total', 'last_update', 'shipment_date', 'tracking_qr_code')
     actions = ['assign_to_rider']
@@ -33,15 +27,14 @@ class CustomerParcelAdmin(ModelAdmin):
     fieldsets = (
         ('Parcel Details', {
             'fields': (
-                ('cn', 'shipper'),
-                ('city', 'customer_payment_status'),
-                ('tracking_qr_code'),
+                ('cn', 'shipper', 'destination'),
+                ('delivery_rider_service_provider', 'api_sp_type'),
+                ('api_tracking_no', 'tpl_payment_status', 'customer_payment_status'),
+                ('assigned_rider', 'tracking_qr_code'),
                 ('consignee', 'consignee_phone'),
                 ('alternate_phone', 'order_number', 'issue_destination'),
-                ('cod', 'rts_cod'),
-                ('parcel_declared_value', 'discount'),
-                ('service_type', 'parcel_weight'),
-                ('number_of_pieces', 'replace'),
+                ('cod', 'rts_cod', 'parcel_declared_value', 'discount'),
+                ('service_type', 'parcel_weight', 'number_of_pieces', 'replace'),
                 ('product', 'instructions'),
                 ('flyer_size', 'flyer_qty'),
                 'address',
@@ -50,8 +43,7 @@ class CustomerParcelAdmin(ModelAdmin):
                 ('active', 'status', 'reason'),
                 'proof_image',
                 ('delivery_date', 'delivery_time', 'share'),
-                ('order_from', 'store_order_number'),
-                ('loadsheet', 'customer_loadsheet'),
+                ('order_from', 'store_order_number', 'loadsheet', 'customer_loadsheet'),
                 ('last_update', 'shipment_date'),
                 ('branch', 'user'),
             )
@@ -59,8 +51,7 @@ class CustomerParcelAdmin(ModelAdmin):
         ('Total Charges', {
             'fields': (
                 'rate_calculation_mode',
-                ('total_gst', 'total_feul_tax'),
-                ('total_return', 'flyer_charges'),
+                ('total_gst', 'total_feul_tax', 'total_return', 'flyer_charges'),
                 ('third_party_charge', 'tpl_net_total'),
                 ('delivery_charge', 'net_total'),
             )
@@ -73,6 +64,9 @@ class CustomerParcelAdmin(ModelAdmin):
         }),
     )
 
+    def payment_status_display(self, obj):
+        return obj.tpl_payment_status
+    payment_status_display.short_description = 'Payment Status'
 
     def assign_to_rider(self, request, queryset):
         from riders.models import Rider
