d559666da9faf285d908ad9353eee422289e9cb9
alialeem01 <ali.aleem.1892@gmail.com>
2026-08-27 16:48:30 +0500

    pickup sheet print setting

--- operations/views.py
+++ operations/views.py
@@ -10,10 +10,11 @@ def track_pickup_sheet(request, sheet_number):
     }
     return render(request, 'tracking/track_pickup_sheet.html', context)
 
+
 def print_pickup_sheet(request, sheet_number):
     sheet = get_object_or_404(PickupSheet, sheet_number=sheet_number)
     context = {
         'sheet': sheet,
-        'parcels': sheet.parcels.all(),
+        'parcels': sheet.parcels.select_related('shipper').all(),
     }
     return render(request, 'tracking/print_pickup_sheet.html', context)
