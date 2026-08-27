5deeb765609de17cd985270eb89ce15d9b485909
alialeem01 <ali.aleem.1892@gmail.com>
2026-08-27 10:24:18 +0500

    Switch to Django's plain ManifestStaticFilesStorage to avoid Whitenoise compression crash on broken third-party file

--- payparcel/storage.py
+++ payparcel/storage.py
@@ -1,10 +1,5 @@
-from whitenoise.storage import CompressedManifestStaticFilesStorage
+from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
 
 
-class SilentCollectstaticStorage(CompressedManifestStaticFilesStorage):
-    def post_process(self, *args, **kwargs):
-        for name, hashed_name, processed in super().post_process(*args, **kwargs):
-            if isinstance(processed, Exception):
-                print(f"Warning: skipping static post-process error for {name}: {processed}")
-                continue
-            yield name, hashed_name, processed
+class SilentCollectstaticStorage(ManifestStaticFilesStorage):
+    manifest_strict = False
