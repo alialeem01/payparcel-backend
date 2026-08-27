fc1962e8ca8346218636c5281f81399d7f941ff5
alialeem01 <ali.aleem.1892@gmail.com>
2026-08-27 10:26:25 +0500

    Fix static storage: use plain ManifestStaticFilesStorage with exception-skipping post_process

--- payparcel/storage.py
+++ payparcel/storage.py
@@ -2,4 +2,9 @@ from django.contrib.staticfiles.storage import ManifestStaticFilesStorage
 
 
 class SilentCollectstaticStorage(ManifestStaticFilesStorage):
-    manifest_strict = False
+    def post_process(self, *args, **kwargs):
+        for name, hashed_name, processed in super().post_process(*args, **kwargs):
+            if isinstance(processed, Exception):
+                print(f"Warning: skipping static post-process error for {name}: {processed}")
+                continue
+            yield name, hashed_name, processed
